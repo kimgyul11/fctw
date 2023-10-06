3383e6d98e800af225094aa18a3cf5e7542a4d85
kimgyul11 <coloo88@naver.com>
2023-10-06 15:21:24 +0900

    update,delete 완료

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,5 +1,36 @@
-import React from "react";
-
+import Loader from "components/loader/Loader";
+import PostBox from "components/posts/PostBox";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
 export default function PostDetail() {
-  return <div>포스트 상세페이지</div>;
+  const params = useParams();
+  const navigate = useNavigate();
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  const getPost = useCallback(async () => {
+    if (params.id) {
+      const docRef = doc(db, "posts", params.id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+    }
+  }, [params.id]);
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  return (
+    <>
+      <div className="post__header">
+        <button type="button" onClick={() => navigate(-1)}>
+          <BiArrowBack className="post__header-btn" />
+        </button>
+      </div>
+      <div className="post">{post ? <PostBox post={post} /> : <Loader />}</div>
+    </>
+  );
 }
